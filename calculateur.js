a6ac0bb99666494a4298826a0785e6c83540a1dc
MarineHerbeth <kayaabeye@gmail.com>
2025-01-08 14:01:08 +0100

    feat: adding calculateur js

---
+++ calculateur.js
@@ -0,0 +1,7 @@
+let button = document.querySelector(".button");
+
+button.addEventListener("click", function () {
+  alert(
+    "Les émissions de CO2 pour les trajts domicile / travail en voiture sont de 12.00kg par mois"
+  );
+});
